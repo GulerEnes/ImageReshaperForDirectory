0674146639a73ef3bd433b0c8d07a3976ba10275
Enes Güler <kriptoenes@gmail.com>
2022-02-01 12:54:40 +0300

    import updates

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from os.path import splitext
 from os import walk
 import easygui as eg
-import cv2 as cv
+from cv2 import imread, imwrite, resize
 
 path = eg.diropenbox()
 fileExt = list(input("File extentions (write comma seperated): ").split(","))
@@ -9,14 +9,14 @@ new_shape = (int(input("width: ")), int(input("height: ")))
 
 try:
     _, _, filenames = next(walk(path))
-    # print(filenames)
+
     for f in filenames:
-        oldName, ext = splitext(f)
+        _, ext = splitext(f)
         if ext[1:] in fileExt:
             filepath = path + '/' + f
-            img = cv.imread(filepath)
-            img = cv.resize(img, new_shape)
-            cv.imwrite(filepath, img)
+            img = imread(filepath)
+            img = resize(img, new_shape)
+            imwrite(filepath, img)
             print("Saved:", filepath)
 
 
